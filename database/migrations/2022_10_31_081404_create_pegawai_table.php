213c250b62c3676def27c871fb66deca2a9a77b5
unknown <naghara15@gmail.com>
2022-11-07 15:42:23 +0700

    auth_patch

---
+++ database/migrations/2022_10_31_081404_create_pegawai_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nama');
+            $table->string('tanggal_lahir');
+            $table->string('divisi');
+            $table->string('karyawan_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+}
